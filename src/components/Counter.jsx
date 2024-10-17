1fc33ff71c8bf4a734ef1ef51abc6ca9f8233e70
Alexander Repeta <alexander.repeta@gmail.com>
2024-10-17 19:34:45 +0300

    Initialize 03-state

---
+++ src/components/Counter.jsx
@@ -0,0 +1,9 @@
+/**
+ * - Зберігає стан count
+ * - Збільшує поточне значення count при onClick
+ * - Рендериться у App
+ */
+
+export default function Counter() {
+  return <button>{/* count */}</button>;
+}
